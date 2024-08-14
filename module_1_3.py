e279f740586ba60ff9f0457f23057f7c6b0dc2f9
nadipetrovna <nadipetrovna@gmail.com>
2024-08-14 23:16:44 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,13 +1,8 @@
-new = 'новое значение'
-str_1 = 'Имя'
-str_2 = 'Возраст'
-colon = ': '
 name = 'Nadezhda'
-str_3 = 'Она студентка:'
-print(str_1,colon,name)
+print(f'My name is:{name}')
 age = 31
-print(str_2,colon,age)
+print(f"I'm {age} years old")
 age = age+1
-print(str_2,colon,new,age)
+print(f"New age is {age}")
 is_studet = True
-print(str_3,colon,is_studet)
+print(f"Is Student: {is_studet}")
