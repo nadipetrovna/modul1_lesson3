eb6003ef87ebe529c3e2e0a3e6a3f19354f832ba
nadipetrovna <nadipetrovna@gmail.com>
2024-08-14 22:52:15 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+new = 'новое значение'
+str_1 = 'Имя'
+str_2 = 'Возраст'
+colon = ': '
+name = 'Nadezhda'
+str_3 = 'Она студентка:'
+print(str_1,colon,name)
+age = 31
+print(str_2,colon,age)
+age = age+1
+print(str_2,colon,new,age)
+is_studet = True
+print(str_3,colon,is_studet)
